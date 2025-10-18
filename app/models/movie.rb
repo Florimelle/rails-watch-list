c2948f16e21d60446db0c10e48c8765e26a51d65
Floriane Diem <florianediem@gmail.com>
2025-10-18 15:10:13 -0400

    seed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,6 @@
 class Movie < ApplicationRecord
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
+
+  has_many :bookmarks, dependent: :restrict_with_error
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'movies/index'
+  get 'movies/new'
+  get 'movies/create'
+  get 'movies/show'
+  get 'movies/update'
+  get 'movies/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -11,5 +17,8 @@ Rails.application.routes.draw do
     resources :bookmarks, only: %i[new create]
   end
 
+  resources :movies do
+    resources :bookmarks, only: %i[new create]
+  end
   resources :bookmarks, only: %i[destroy]
 end
