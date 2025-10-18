fc2c22c347f6fe8d04bbbeb8e78fe94932326e66
Floriane Diem <florianediem@gmail.com>
2025-10-18 15:59:04 -0400

    all_done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,5 @@
 class Movie < ApplicationRecord
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
-
-  has_many :bookmarks, dependent: :restrict_with_error
+  has_many :bookmarks
 end
